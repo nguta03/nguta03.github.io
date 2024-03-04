08b5aef106580a7dc9df577e7d69380ee143a181
nguta03 <noahguta03@gmail.com>
2024-03-03 22:54:13 -0500

    formatting

--- index.js
+++ index.js
@@ -7,31 +7,6 @@ $(document).ready(function(){
     $("header").css({ height: $(window).height() });
     $("body").css({ width: $(window).width() });
   });
-    /**
-     * Easy selector helper function
-     */
-    const select = (el, all = false) => {
-      el = el.trim()
-      if (all) {
-        return [...document.querySelectorAll(el)]
-      } else {
-        return document.querySelector(el)
-      }
-    }
-  
-    /**
-     * Easy event listener function
-     */
-    const on = (type, el, listener, all = false) => {
-      let selectEl = select(el, all)
-      if (selectEl) {
-        if (all) {
-          selectEl.forEach(e => e.addEventListener(type, listener))
-        } else {
-          selectEl.addEventListener(type, listener)
-        }
-      }
-    }
   
     /**
      * Easy on scroll event listener 
@@ -76,38 +51,6 @@ $(document).ready(function(){
         top: elementPos - offset,
         behavior: 'smooth'
       })
-    }
-  
-    /**
-     * Toggle .header-scrolled class to #header when page is scrolled
-     */
-    let selectHeader = select('#header')
-    if (selectHeader) {
-      const headerScrolled = () => {
-        if (window.scrollY > 100) {
-          selectHeader.classList.add('header-scrolled')
-        } else {
-          selectHeader.classList.remove('header-scrolled')
-        }
-      }
-      window.addEventListener('load', headerScrolled)
-      onscroll(document, headerScrolled)
-    }
-  
-    /**
-     * Back to top button
-     */
-    let backtotop = select('.back-to-top')
-    if (backtotop) {
-      const toggleBacktotop = () => {
-        if (window.scrollY > 100) {
-          backtotop.classList.add('active')
-        } else {
-          backtotop.classList.remove('active')
-        }
-      }
-      window.addEventListener('load', toggleBacktotop)
-      onscroll(document, toggleBacktotop)
     }
   
     /**
@@ -173,9 +116,7 @@ $(document).ready(function(){
         backDelay: 2000
       });
     }
-    
-  
-  
+
     /**
      * Preloader
      */
